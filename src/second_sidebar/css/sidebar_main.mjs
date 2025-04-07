542ef730777fdc4c768bc497920153c7e0652b30
k00lagin <k00lagin@gmail.com>
2025-04-07 13:53:20 +0500

    Preserve floating sidebar when webpanel is opened

--- src/second_sidebar/css/sidebar_main.mjs
+++ src/second_sidebar/css/sidebar_main.mjs
@@ -75,6 +75,7 @@ export const SIDEBAR_MAIN_CSS = `
     transform: translateX(calc(-100% + 4px));
   }
 
+  #browser:has(#sb2[floating="true"]:not([pinned="true"])):has(#sb2-box:not([hidden="true"])) #sb2-main,
   #browser:has(#sb2[floating="true"]) #sb2-main:hover,
   #browser:has(#sb2[floating="true"]) #sb2-main:focus-within {
     transform: translateX(0) !important;
