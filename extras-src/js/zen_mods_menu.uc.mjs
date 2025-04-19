5afc533348fce22db495863b56af60f1557bde7d
k00lagin <k00lagin@gmail.com>
2025-04-20 02:27:50 +0500

    Add extensions menu script
    (cherry picked from commit 81b019ad3752c511ef3f00eacd2972a90d075b33)

--- extras-src/js/zen_mods_menu.uc.mjs
+++ extras-src/js/zen_mods_menu.uc.mjs
@@ -5,81 +5,81 @@
 // @include         about:preferences#zenMarketplace
 // ==/UserScript==
 
-console.log("Zen Mods Menu");
-
-const removeElement = (element) => {
-    element.remove();
-}
-
-const removeElements = (elements) => {
-    Array.from(elements).forEach(removeElement);
-}
-
-const run = () => {
+const run = () => { 
     if (navigator.userAgent.toLowerCase().includes("mobile")) {
-        const css = `
-        body {
-            overflow-y: auto !important;
-        }
+        const css = /*css*/`
+            body {
+                overflow-y: auto !important;
+            }
 
-        .main-content,
-        #zenMarketplaceGroup {
-            display: contents;        
-        }
+            .main-content,
+            #zenMarketplaceGroup {
+                display: contents;        
+            }
+            
+            .main-content > .pane-container {
+                margin: 0;
+            }
+
+            .navigation,
+            #ZenMarketplaceCategory,
+            [data-l10n-id="zen-theme-marketplace-description"],
+            [data-l10n-id="zen-theme-marketplace-link"],
+            [data-l10n-id="zen-theme-marketplace-check-for-updates-button"],
+            .zenThemeMarketplaceItemActions,
+            .zenThemeMarketplaceItemDescription {
+                display: none;
+            }
+
+            #mainPrefPane {
+                padding: var(--space-small) !important;
+            }
+
+            #zenMarketplaceHeader {
+                border-radius: 5px;
+                padding: var(--space-medium);
+                justify-content: space-between;
+                align-items: center;
+                flex-direction: row;
+                cursor: default;
+            }
+
+            #zenMarketplaceHeader > * {
+                font-size: var(--font-size-small);
+                opacity: 1;
+            }
+            
+            #zenMarketplaceGroup {
+                background-color: transparent;
+            }
+
+            #zenThemeMarketplaceList {
+                display: contents;
+            }
+
+            .zenThemeMarketplaceItem {
+                border-width: 0;
+            }
+
+            .zenThemeMarketplaceItem label {
+                margin: 0;
+            }
+
+            #zenMarketplaceHeader:hover,
+            .zenThemeMarketplaceItem:hover {
+                background-color: color-mix(in srgb, var(--toolbarbutton-hover-background) 50%, transparent 50%);
+            }
+
+            .zenThemeMarketplaceItem h3 {
+                font-size: var(--font-size-small);
+            }
+
+            toolbarseparator {
+                border-top: 1px solid var(--panel-separator-color);
+                margin: var(--space-small) 0;
+            }
+        `;
         
-        .main-content > .pane-container {
-            margin: 0;
-        }
-
-        .navigation,
-        #ZenMarketplaceCategory,
-        [data-l10n-id="zen-theme-marketplace-description"],
-        [data-l10n-id="zen-theme-marketplace-link"],
-        [data-l10n-id="zen-theme-marketplace-check-for-updates-button"],
-        .zenThemeMarketplaceItemActions,
-        .zenThemeMarketplaceItemDescription {
-            display: none;
-        }
-
-        #mainPrefPane {
-            padding: var(--space-small) !important;
-        }
-
-        #zenMarketplaceHeader {
-            border-radius:
-            5px;
-            padding: var(--space-small);
-            justify-content: space-between;
-            align-items: center;
-            flex-direction: row;
-        }
-        
-        #zenMarketplaceGroup {
-            background-color: transparent;
-        }
-
-        #zenThemeMarketplaceList {
-            display: contents;
-        }
-
-        .zenThemeMarketplaceItem {
-            border-width: 0;
-        }
-
-        #zenMarketplaceHeader:hover,
-        .zenThemeMarketplaceItem:hover {
-            background-color: color-mix(in srgb, var(--toolbarbutton-hover-background) 50%, transparent 50%);
-        }
-
-        .zenThemeMarketplaceItem h3 {
-            font-size: var(--font-size-small);
-        }
-
-        toolbarseparator {
-            border-top: 1px solid var(--panel-separator-color);
-            margin: var(--space-small) 0;
-        }
-    `;
         const style = document.createElement("style");
         style.textContent = css;
         document.head.appendChild(style);
@@ -91,9 +91,7 @@ const run = () => {
 };
 
 if (typeof UC_API !== "undefined") {
-    console.log("Zen Mods Menu  UC_API finished");
     UC_API.Runtime.startupFinished().then(run);
 } else {
-    console.log("Zen Mods Menu  delayedStartupPromise");
     delayedStartupPromise.then(run);
 }
