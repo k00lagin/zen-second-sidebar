49797e2ea56838ace9790bf34c98276b087a955e
k00lagin <k00lagin@gmail.com>
2025-05-20 13:42:13 +0500

    Make sidebar transparent by default
    When in not compact mode. By removing leftover class

--- src/second_sidebar/xul/sidebar_main.mjs
+++ src/second_sidebar/xul/sidebar_main.mjs
@@ -2,7 +2,7 @@ import { Toolbar } from "./base/toolbar.mjs";
 
 export class SidebarMain extends Toolbar {
   constructor() {
-    super({ id: "sb2-main", classList: ["browser-toolbox-background"] });
+    super({ id: "sb2-main" });
     this.setMode("icons")
       .setContext("sb2-main-menupopup")
       .setAttribute("customizable", "true")
